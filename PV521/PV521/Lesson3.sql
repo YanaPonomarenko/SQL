5f291478548d7dfca491f1b140073e4ced05f4f3
Yana Ponomarenko <y.ponomar05@gmail.com>
2026-02-15 13:34:26 +0200

    added practice 09.02

--- PV521/PV521/Lesson3.sql
+++ PV521/PV521/Lesson3.sql
@@ -1,4 +1,4 @@
-USE pv521;
+ÔªøUSE pv521;
 
 CREATE TABLE authors(
   id INT PRIMARY KEY IDENTITY(1,1),
@@ -23,25 +23,25 @@ CREATE TABLE books(
 
 INSERT INTO authors (name, surname)
 VALUES 
-('“‡‡Ò', 'ÿÂ‚˜ÂÌÍÓ'),
-('≤‚‡Ì', '‘‡ÌÍÓ'),
-('ÀÂÒˇ', '”Í‡øÌÍ‡'),
-('ÃËı‡ÈÎÓ', ' Óˆ˛·ËÌÒ¸ÍËÈ');
+('–¢–∞—Ä–∞—Å', '–®–µ–≤—á–µ–Ω–∫–æ'),
+('–Ü–≤–∞–Ω', '–§—Ä–∞–Ω–∫–æ'),
+('–õ–µ—Å—è', '–£–∫—Ä–∞—ó–Ω–∫–∞'),
+('–ú–∏—Ö–∞–π–ª–æ', '–ö–æ—Ü—é–±–∏–Ω—Å—å–∫–∏–π');
 
 INSERT INTO genres (name)
 VALUES
-('œÓÂÁ≥ˇ'),
-('–ÓÏ‡Ì'),
-('ƒ‡Ï‡'),
-('ÕÓ‚ÂÎ‡');
+('–ü–æ–µ–∑—ñ—è'),
+('–†–æ–º–∞–Ω'),
+('–î—Ä–∞–º–∞'),
+('–ù–æ–≤–µ–ª–∞');
 
 INSERT INTO books (title, [year], author_id, genre_id)
 VALUES
-(' Ó·Á‡', 1840, 1, 1),
-('√‡È‰‡Ï‡ÍË', 1841, 1, 1),
-('«‡ı‡ ¡ÂÍÛÚ', 1883, 2, 2),
-('ÃÓÈÒÂÈ', 1905, 2, 1),
-('À≥ÒÓ‚‡ Ô≥ÒÌˇ', 1911, 3, 3),
+('–ö–æ–±–∑–∞—Ä', 1840, 1, 1),
+('–ì–∞–π–¥–∞–º–∞–∫–∏', 1841, 1, 1),
+('–ó–∞—Ö–∞—Ä –ë–µ—Ä–∫—É—Ç', 1883, 2, 2),
+('–ú–æ–π—Å–µ–π', 1905, 2, 1),
+('–õ—ñ—Å–æ–≤–∞ –ø—ñ—Å–Ω—è', 1911, 3, 3),
 ('Intermezzo', 1908, 4, 4);
 
 
@@ -50,3 +50,42 @@ FROM books AS b, authors AS a, genres AS g
 WHERE b.author_id = a.id AND b.genre_id = g.id
 ORDER BY b.[year] ASC;
 
+ALTER TABLE books
+ADD price DECIMAL(10,2);
+
+UPDATE books SET price = 250.00 WHERE title = '–ö–æ–±–∑–∞—Ä';
+UPDATE books SET price = 200.00 WHERE title = '–ì–∞–π–¥–∞–º–∞–∫–∏';
+UPDATE books SET price = 180.00 WHERE title = '–ó–∞—Ö–∞—Ä –ë–µ—Ä–∫—É—Ç';
+UPDATE books SET price = 220.00 WHERE title = '–ú–æ–π—Å–µ–π';
+UPDATE books SET price = 300.00 WHERE title = '–õ—ñ—Å–æ–≤–∞ –ø—ñ—Å–Ω—è';
+UPDATE books SET price = 150.00 WHERE title = 'Intermezzo';
+
+SELECT DISTINCT title FROM books;
+
+SELECT TOP 5 * FROM books;
+
+SELECT TOP 1 * FROM books ORDER BY price DESC;
+
+SELECT AVG(price) AS average_price_after_1830 FROM books  WHERE year > 1830;
+
+SELECT 
+    a.name, 
+    a.surname, 
+    SUM(b.price) AS total_price
+FROM books AS b
+INNER JOIN authors AS a ON a.id = b.author_id
+GROUP BY a.id, a.name, a.surname
+ORDER BY total_price DESC;
+
+
+SELECT  a.name, a.surname, COUNT(author_id) AS count_books 
+FROM books AS b
+INNER JOIN authors AS a
+ON a.id = b.author_id
+GROUP BY author_id, a.name, a.surname
+HAVING a.name LIKE N'–¢–∞—Ä–∞—Å' OR a.name LIKE N'–õ–µ—Å—è';
+
+SELECT year, COUNT(year) AS books_count 
+FROM books
+GROUP BY year
+ORDER BY year;
